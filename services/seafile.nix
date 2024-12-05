01261a1423693eefcb418fc47435c85f922a6e38
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-12-05 18:15:09 +0100

    seafile werkt met revers proxy

--- services/seafile.nix
+++ services/seafile.nix
@@ -1,22 +1,28 @@
 { config, lib, pkgs, ... }:
-{
+let url = "seafile.martijnv.com"; in {
   services.seafile = {
+    # enable = false;
     enable = true;
     adminEmail = "martijnvoordouw@gmail.com";
-    seahubAddress = "0.0.0.0:8083";
-    # dataDir = "/path";
-    # group = "";
+    seahubAddress = "127.0.0.1:8083";
+    # setting dataDir seems to be broken
+    # dataDir = "/datadisk/seafile/data";
     initialAdminPassword = "wachtwoord"; #TODO
     # seafileSettings = "https://manual.seafile.com/config/seafile-conf/";
     seafileSettings = {
       fileserver = {
         port = 8082;
-        host = "ipv4:0.0.0.0";
+        host = "ipv4:127.0.0.1";
       };
     };
     gc.enable = true; # TODO garbage collection?
-    ccnetSettings.General.SERVICE_URL = "http://0.0.0.0:8083/"
-    # user = "seafile";
+    ccnetSettings.General.SERVICE_URL = "https://${url}";
+    user = "seafile";
+    group = "seafile";
     # workers = 4;
   };
+
+  services.caddy.virtualHosts."${url}".extraConfig = ''
+    reverse_proxy localhost:8083
+  '';
 }
