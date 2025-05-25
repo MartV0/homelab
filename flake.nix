c21d5e35465a0cd7929286a5ea35b6e807b0b40b
MartV0 <martijnvoordouw@gmail.com>
2025-05-25 18:10:21 +0200

    Updated to nixos 25.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nixos flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     staticSite.url = "github:MartV0/personal-site";
     agenix.url = "github:ryantm/agenix";
@@ -17,14 +17,20 @@
       in {
     nixosConfigurations = {
       ${hostnamepi} = nixpkgs.lib.nixosSystem rec {
-        specialArgs = { inherit flakePath inputs staticSite username agenix; hostname=hostnamepi; system=system_aarch; pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-};
+        specialArgs = { 
+          inherit flakePath inputs staticSite username agenix;
+          hostname=hostnamepi; system=system_aarch;
+          pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
+        };
         system = system_aarch;
         modules = [ ./machines/pi4/configuration.nix agenix.nixosModules.default ];
       };
       ${hostnamenuc} = nixpkgs.lib.nixosSystem rec {
-        specialArgs = { inherit flakePath inputs staticSite username agenix; hostname=hostnamenuc; system=system_x86; pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-};
+        specialArgs = {
+          inherit flakePath inputs staticSite username agenix;
+          hostname=hostnamenuc; system=system_x86;
+          pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
+        };
         system = system_x86;
         modules = [ ./machines/nuc/configuration.nix agenix.nixosModules.default ];
       };
