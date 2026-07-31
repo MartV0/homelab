2d0dec4192983c90771756816908127f442700ef
MartV0 <martijnvoordouw@gmail.com>
2026-07-31 14:17:38 +0200

    Replaced mullvad with proton vpn

--- modules/applications/vpn.nix
+++ modules/applications/vpn.nix
@@ -1,21 +1,10 @@
 { pkgs-unstable, ... }:
 {
   environment.systemPackages = with pkgs-unstable; [ 
-      mullvad
-      mullvad-vpn
-      mullvad-browser
+    proton-vpn
+    proton-vpn-cli
   ];
 
-  services.mullvad-vpn.enable = true;
-  services.mullvad-vpn.package = pkgs-unstable.mullvad-vpn;
-
-  networking.wireguard.enable = true;
-
-  networking.firewall = {
-    checkReversePath = "loose";
-    allowedUDPPorts = [ 51820 ];
-  };
-
-  services.resolved.enable = true;
+  networking.firewall.checkReversePath = "loose";
 }
 
